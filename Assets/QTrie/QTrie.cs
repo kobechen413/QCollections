994ddbfdf5c590e97584335915ae33ab113d07c5
chenqiang <huoqilin@163.com>
2018-09-03 18:13:11 +0800

    添加字典树

---
+++ Assets/QTrie/QTrie.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QCollections
+{
+    // 字典树
+    public class QTrie
+    {
+        private TrieNode m_Root;
+
+        private int m_Size;
+
+        public QTrie(int size)
+        {
+            m_Size  = size;
+            m_Root = new TrieNode(size);
+        }
+
+        // 插入单词
+        public bool Insert(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+
+            TrieNode pNode = m_Root;
+            char[] wordArr = word.ToCharArray();
+            for (int i = 0, length = wordArr.Length; i < length; i++)
+            {
+                int pos = wordArr[i] - 'a';
+                if (pNode.childNodes[pos] == null)
+                {
+                    pNode.childNodes[pos] = new TrieNode(m_Size);
+                    pNode.childNodes[pos].data = wordArr[i];
+                }
+                else
+                {
+                    ++pNode.childNodes[pos].count;
+                }
+                pNode = pNode.childNodes[pos];
+            }
+
+            pNode.isEnd = true;
+
+            return true;
+        }
+
+        // 计算单词前缀的数量
+        public int WordPrefixCount(string prefix)
+        {
+            if (string.IsNullOrEmpty(prefix))
+            {
+                return -1;
+            }
+
+            TrieNode pNode = m_Root;
+            char[] wordArr = prefix.ToCharArray();
+            for (int i = 0, length = wordArr.Length; i < length; i++)
+            {
+                int pos = wordArr[i] - 'a';
+                if (pNode.childNodes[pos] == null)
+                {
+                    return 0;
+                }
+                pNode = pNode.childNodes[pos];
+            }
+
+            return pNode.count;
+        }
+
+        // 是否包含完整单词
+        public bool Contain(string word)
+        {
+            TrieNode pNode = m_Root;
+            char[] wordArr = word.ToCharArray();
+            for (int i = 0, length = wordArr.Length; i < length; i++)
+            {
+                int pos = wordArr[i] - 'a';
+                if (pNode.childNodes[pos] == null)
+                {
+                    return false;
+                }
+                pNode = pNode.childNodes[pos];
+            }
+
+            return pNode.isEnd;
+        }
+
+        // 遍历字典树接口
+        public void PreOrder()
+        {
+            PreOrder(m_Root);
+        }
+
+        // 遍历字典树
+        public void PreOrder(TrieNode pNode)
+        {
+            if (pNode != null)
+            {
+                Debug.LogFormat("遍历字典树节点 => {0}", pNode.data);
+                foreach (TrieNode child in pNode.childNodes)
+                {
+                    PreOrder(child);
+                }
+            }
+        }
+
+
+        public class TrieNode
+        {
+            // 字典树节点数据
+            public char data;
+            // 子节点
+            public TrieNode[] childNodes;
+            // 经过该节点的数量
+            public int count;
+            // 是否是最后一个节点
+            public bool isEnd;
+
+            public TrieNode(int size)
+            {
+                count = 1;
+                isEnd = false;
+                childNodes = new TrieNode[size];
+            }
+        }
+
+
+    }
+}
+
---
+++ Assets/QTrie/UTQTrie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QCollections
+{
+    public class UTQTrie : MonoBehaviour
+    {
+        private readonly int SIZE = 26;
+        // Use this for initialization
+        void Start()
+        {
+            QTrie trie = new QTrie(SIZE);
+
+            IList<string> wordLst = new List<string>()
+            {
+				"abc", "abcd", "xyz", "dba", "cde", "wahaha", "james", "abcd", "dbc", "dba", "abcdef", "abc"
+            };
+
+            for (int i = 0; i < wordLst.Count; i++)
+			{
+                trie.Insert(wordLst[i]);
+            }
+
+            Debug.LogFormat("abc 子串前缀数量 => {0}", trie.WordPrefixCount("abc"));
+
+			Debug.LogFormat("是否包含单词 wahaha => {0}", trie.Contain("wahaha"));
+
+			trie.PreOrder();
+
+        }
+
+    }
+
+}
